bd90d76a54f51eae1f03355a74cf0838f8471b34
Daniel <danielparra93@gmail.com>
2016-08-08 20:04:30 -0400

    front angular setup completed

---
+++ public/js/controllers/SimpleController.js
@@ -0,0 +1,10 @@
+(function(){
+  angular.module('ngBlog')
+          .controller('SimpleController', SimpleController);
+
+  SimpleController.$inject = ['$scope'];
+
+  function SimpleController($scope){
+    $scope.message = 'Hey! Angular Works!';
+  }
+})();
